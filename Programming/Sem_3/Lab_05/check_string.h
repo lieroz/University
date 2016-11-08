6f5deb01db50ab4fdb90032ca15b6adf200bf263
lieroz <a.kamakin@icloud.com>
2016-11-09 00:46:35 +0300

    Added programming lab №5

---
+++ Programming/Sem_3/Lab_05/check_string.h
@@ -0,0 +1,11 @@
+#ifndef __CHECK_STRING_H_DEF__
+#define __CHECK_STRING_H_DEF__
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+void CheckNumbers(FILE*); 
+
+#endif // __CHECK_STRING_H_DEF__
