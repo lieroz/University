484c6cc64f40f353af2e2750d89e16ae2bc79b67
lieroz <a.kamakin@icloud.com>
2016-12-02 23:47:31 +0300

    Restructuring of program.

---
+++ Programming/Sem_3/Lab_10/graph.c
@@ -0,0 +1,44 @@
+//
+// Created by lieroz on 02.12.16.
+//
+
+#include "graph.h"
+
+// A utility function to create a new adjacency list node
+AdjListNode* NewAdjListNode(int dest, int weight) {
+	AdjListNode* new_node = (AdjListNode*) malloc(sizeof(struct AdjListNode));
+
+	new_node->dest = dest;
+	new_node->weight = weight;
+	new_node->next = NULL;
+
+	return new_node;
+}
+
+Graph* CreateGraph(int V) {
+	Graph* graph = (Graph*) malloc(sizeof(Graph));
+	graph->V = V;
+
+	// Create an array of adjacency lists.  Size of array will be V
+	graph->array = (AdjList*) malloc(V * sizeof(AdjList));
+
+	// Initialize each adjacency list as empty by making head as NULL
+	for (int i = 0; i < V; ++i) {
+		graph->array[i].head = NULL;
+	}
+
+	return graph;
+}
+
+void AddEdge(Graph* graph, int src, int dest, int weight) {
+	// Add an edge from src to dest.  A new node is added to the adjacency
+	// list of src.  The node is added at the begining
+	AdjListNode* new_node = NewAdjListNode(dest, weight);
+	new_node->next = graph->array[src].head;
+	graph->array[src].head = new_node;
+
+	// Since graph is undirected, add an edge from dest to src also
+	new_node = NewAdjListNode(src, weight);
+	new_node->next = graph->array[dest].head;
+	graph->array[dest].head = new_node;
+}
