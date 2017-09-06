ec8596a6c1893789a6b22c64ba577059c2f54596
Andrey Kamakin <a.kamakin@icloud.com>
2017-09-06 22:02:19 +0300

    Changed for PWD

--- Sem_05/DbCourse/Lab_01/run.sh
+++ Sem_05/DbCourse/Lab_01/run.sh
@@ -2,7 +2,7 @@
 
 python3 data_generator.py
 
-sqlcmd -S localhost -U SA -P Parol123456 -i /home/lieroz/University/Sem_05/DbCourse/Lab_01/V0_db_init.sql
-sqlcmd -S localhost -U SA -P Parol123456 -i /home/lieroz/University/Sem_05/DbCourse/Lab_01/V1_init.sql
-sqlcmd -S localhost -U SA -P Parol123456 -i /home/lieroz/University/Sem_05/DbCourse/Lab_01/V2_alter.sql
-sqlcmd -S localhost -U SA -P Parol123456 -i /home/lieroz/University/Sem_05/DbCourse/Lab_01/V3_insert.sql
+sqlcmd -S localhost -U SA -P Parol123456 -i $PWD/V0_db_init.sql
+sqlcmd -S localhost -U SA -P Parol123456 -i $PWD/V1_init.sql
+sqlcmd -S localhost -U SA -P Parol123456 -i $PWD/V2_alter.sql
+sqlcmd -S localhost -U SA -P Parol123456 -i $PWD/V3_insert.sql
