62d64f75148ac08a8545a1abc0ecd465cdf14472
Andrey Kamakin <a.kamakin@icloud.com>
2017-11-01 01:34:49 +0300

    removed

--- Sem_05/DbCourse/Lab_01/V0_db_init.sql
+++
@@ -1,8 +0,0 @@
-USE master;
-
-IF EXISTS(SELECT name
-          FROM sys.databases
-          WHERE name = 'Travels')
-  DROP DATABASE Travels;
-
-CREATE DATABASE Travels;
--- Sem_05/DbCourse/Lab_01/run.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-
-python3 data_generator.py
-
-sqlcmd -S localhost -U SA -P Parol123456 -i $PWD/V0_db_init.sql
-sqlcmd -S localhost -U SA -P Parol123456 -i $PWD/V1_init.sql
-sqlcmd -S localhost -U SA -P Parol123456 -i $PWD/V2_alter.sql
-sqlcmd -S localhost -U SA -P Parol123456 -i $PWD/V3_insert.sql
