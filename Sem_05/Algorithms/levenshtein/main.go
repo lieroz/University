cce6b54ff9c9c2b59da1393afc9716d8e432c9e4
Andrey Kamakin <a.kamakin@icloud.com>
2017-11-01 03:36:41 +0300

    cleaning

--- Sem_05/Algorithms/levenshtein/main.go
+++ Sem_05/Algorithms/levenshtein/main.go
@@ -1,9 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const (
+	first  = "cpdkU55pbN"//NO7QzCq3JDSF0cR1tfyNqTWsj6FicWbwufJeHhAWhvds8mciYEQWaiEZuxnH1FbmnDSXsaNN6Bam6Bs54a1p8MR1Aw"
+	second = "NoQSy5cTDK"//BtxFQ70jmtGHsF2Ad7d9jTITwutxphwPQ9DMBzjaPNnMbeiGi6FGalheQEPMZW0QfMrP8GX2isjMhy7NjBPypH8GKL"
+)
 
 func main() {
-	fmt.Println(LevenshteinIteraitive("abcdefg", "badclth"))
-	fmt.Println(LevenshteinRecursive("abcdefg", "badclth"))
-	fmt.Println(LevenshteinOptimized("abcdefg", "badclth"))
+	fmt.Println(LevenshteinIteraitive(first, second))
+	fmt.Println(LevenshteinRecursive(first, second))
+	fmt.Println(LevenshteinOptimized(first, second))
 }
